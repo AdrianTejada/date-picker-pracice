488700c936fa6c7bfbe0befe3dd215e11a726a1b
AdrianTejada <atejada@my.bcit.ca>
2023-03-17 21:21:08 -0700

    installed date-fns

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DatePickerAI from './Components/DatePickerAI'
+import {DatePicker} from './Components/DatePicker';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <DatePickerAI onSelectDate={(event)=>console.log(event)}/>
+      {/* <DatePickerAI onSelectDate={(event)=>console.log(event)}/> */}
+      <DatePicker/>
     </div>
   )
 }
--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -1,20 +1,98 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { BsChevronLeft } from "react-icons/bs";
+import { BsChevronRight } from "react-icons/bs";
+import { 
+  startOfToday,
+  format,
+  eachDayOfInterval,
+  startOfMonth,
+  endOfMonth,
+} from 'date-fns';
+
 const Container = styled.div`
-    width: 346px;
-    height: 300px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  width: 346px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f6f6f6;
+  border-radius: 5px;
 `;
 
+const Top = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 36px;
+`
+
 const Month = styled.h3`
-    
+  font-family: 'RM';
+  font-weight: 400;
+  font-size: 16px;
+  color: #333;
+`
+
+
+const Week = styled.div`
+  width: 100%;
+  height: 31px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom:21px;
+`
+
+const Cell = styled.div`
+  width: 31px;
+  height: 31px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Weekday = styled.div`
+  width: 31px;
+  height: 31px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'RM';
+  font-size: 13px;
+  cursor: default;
+  font-weight: 400;
+  /* border: 1px solid black; */
 `
 
 export const DatePicker = () => {
-  return <Container>
+  let today = startOfToday()
 
+  let newDays = eachDayOfInterval({start: startOfMonth(today), end: endOfMonth(today)})
+
+  return <Container>
+    <Top>
+      <BsChevronLeft size={20} color='#333333'/>
+        <Month>
+          {format(today, 'MMMM yyyy')}
+        </Month>
+      <BsChevronRight size={20} color='#333333'/>
+    </Top>
+    <Week>
+      <Weekday>Su</Weekday>
+      <Weekday>Mo</Weekday>
+      <Weekday>Tu</Weekday>
+      <Weekday>We</Weekday>
+      <Weekday>Th</Weekday>
+      <Weekday>Fr</Weekday>
+      <Weekday>Sa</Weekday>
+    </Week>
+    <Row>
+
+    </Row>
+    <button onClick={()=>console.log(newDays)}>show days</button>
   </Container>
 }
